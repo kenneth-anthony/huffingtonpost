4d6e364111f86d536c15b8f72ba92190a2e851a4
Kenneth Chavez <kchavez.bills@gmail.com>
2015-07-15 15:14:44 -0600

    Added .index and .gitignore           Added HTML           Added <p> text

--- .idea/index.php
+++ .idea/index.php
@@ -6,6 +6,13 @@
 	<body>
 		<h1>DATA DESIGN PROJECT</h1>
 		<h4>Huffington Post</h4>
+		<h6>Persona</h6>
+		<p> The persona for Huffington Post is a educated, liberal, 18-42, <br>
+			65% female, 35% male audience. The typical Huffington Post user<br>
+			is tech savey and views on a mobile device. The end user wants to read <br>
+			politics, busines, entertainment, tech, media, worldpost, healthy, and <br>
+			and occasionally enjoy comedy.
+		</p>
 	</body>
 
 </html>
