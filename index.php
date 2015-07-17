f131b5d4155773bad89b38ef9b2ef19612f07b8a
Kenneth Chavez <kchavez.bills@gmail.com>
2015-07-16 22:41:50 -0600

    changed style sheet name to "mystyle.css pushing data so I can use my home office

--- index.php
+++ index.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
-<html lang="eng" xmlns="http://www.w3.org/1999/html">
-		<head>
-			<meta charset="utf-8">
-			<link rel="stylesheet" type="text/css" href="mystyle.css">
-		</head>
-	<body>
-		<h1>DATA DESIGN PROJECT</h1>
-		<h2>Huffington Post</h2>
-		<h4>Persona</h4>
-		<p>The persona for Huffington Post is a educated, liberal, 18-42, <br>
-			65% female, 35% male audience. The typical Huffington Post user<br>
-			is tech savvy and views HP on a mobile device. The end user wants to read <br>
-			read politics, business, entertainment, tech, media, worldpost, healthy, and <br>
-			and occasionally enjoys comedy.<br>
-			 To read further demographic information click the link<br>
-			<br>
-		<h4>Use Case</h4>
-	</body>
+	<head>
+		<meta charset="utf-8">
+		<Link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+
+<body>
+	<div class="container">
+		<header>
+			<h1>Huffington Post</h1>
+			<h3>Data Design Project</h3>
+		</header>
+	</div>
+</body>
 
 </html>
