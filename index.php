31add2bfbe0e12bfc554d560598f3a2c58fb7a5e
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-07-17 23:38:26 -0600

    Added images

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 	<head>
 		<meta charset="utf-8">
 		<Link rel="stylesheet" type="text/css" href="mystyle.css">
-		<!--Added google fonts -->
 		<link href='http://fonts.googleapis.com/css?family=Gravitas+One|Droid+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 </head>
 
@@ -12,6 +11,7 @@
 			<h1>Huffington Post</h1>
 			<p>Data Design Project</p>
 		</header>
+		<div	class="backgroundImage"></div>
 	</div>
-	<p><a href="images/old_paper_texture_by_caminopalmero.jpg"</a> </p>
+
 </body>
