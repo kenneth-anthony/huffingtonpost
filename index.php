ac7ec8f8d2ee37013c665856b6f49c4038a3019e
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-07-18 09:42:44 -0600

    Added images

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <!DOCTYPE html>
-<html lang="en	"
-<head>
-	<meta charset="utf-8">
-	<Link rel="stylesheet" type="text/css" href="huffStyle.css">
-	<link href='http://fonts.googleapis.com/css?family=Gravitas+One|' rel='stylesheet' type='text/css'>
-</head>
+<html>
+	<body>
 
-<body>
-	<div class="container">
-		<header>
-			<h1>Huffington Post</h1>
-			<p>Data Design Project</p>
-		</header>
-		<div	class="backgroundImage"></div>
-	</div>
+		<h1>My First Heading</h1>
+		<p>My first paragraph.</p>
+
+	</body>
 </html>
 
-</body>
+
