a127fd3f31497f698477d77e1a71ecb9e370f689
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-07-19 14:29:37 -0600

    Added images

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
-<html>
-	<body>
+<head xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+	<meta charset="utf-8">
+	<Link rel="stylesheet" type="text/css" href="huffStyle.css">
+	<link href='http://fonts.googleapis.com/css?family=Gravitas+One' rel='stylesheet' type='text/css'>
+</head>
 
-		<h1>My First Heading</h1>
-		<p>My first paragraph.</p>
+<body>
+	<div class="container">
+		<header>
+			<h1>Huffington Post</h1>
+			<p>Data Design Project</p>
+		</header>
+	</div>
+	<div class="background">
+		<img src="images/old_paper_texture_by_caminopalmero.jpg">
+	</div>
 
-	</body>
-</html>
+	<main>
+		
+	</main>
 
-
+</body>
